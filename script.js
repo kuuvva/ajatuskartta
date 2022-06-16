0166bdacb09bbd3e9418d8bf482170353637f235
kuuvva <kuuvva@proton.me>
2022-06-16 10:28:46 +0300

    aloitus

---
+++ script.js
@@ -0,0 +1,5 @@
+"use strict";
+// Dynaamisesti luotu ajatuskartta menetetään sivulta poistuttaessa,
+// joten kysytään varmistus.
+// window.addEventListener("beforeunload", e => e.preventDefault());
+console.log("hello");
