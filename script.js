d53a4c2f9b4db8435285ea393451c4d1a7afcad5
kuuvva <kuuvva@proton.me>
2022-06-17 22:34:23 +0300

    Solmun luonti sijaintiin body-elementissä.

--- script.js
+++ script.js
@@ -2,4 +2,23 @@
 // Dynaamisesti luotu ajatuskartta menetetään sivulta poistuttaessa,
 // joten kysytään varmistus.
 // window.addEventListener("beforeunload", e => e.preventDefault());
-console.log("hello");
+
+/**
+ * Luodaan solmuelementti annettuun sijaintiin
+ * body-elementissä.
+ * @param {number} posX - vasemman reunan x-koordinaatti bodyssa
+ * @param {number} posY - yläreunan y-koordinaatti bodyssa
+ * @returns {HTMLDivElement} luotu solmu
+ */
+function solmu(posX, posY) {
+
+    const solmu = document.createElement("div");
+
+    const solmuClass = "solmu";
+    solmu.classList.add(solmuClass);
+
+    document.body.appendChild(solmu);
+    solmu.style.left = posX + "px";
+    solmu.style.top = posY + "px";
+    return solmu;
+}
